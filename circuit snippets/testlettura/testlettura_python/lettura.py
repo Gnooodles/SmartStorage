cb24e724927d90a4dc49cf4def95bd5f96b95a13
Daniele Gnudi <62075147+Gnooodles@users.noreply.github.com>
2023-08-08 12:09:10 +0200

    try Python read barcode

--- circuit snippets/testlettura/testlettura_python/lettura.py
+++ circuit snippets/testlettura/testlettura_python/lettura.py
@@ -1,5 +1,22 @@
 from arduino_functions import *
-import time
+import time, serial
+from pyfirmata import util
 
-board = setup('/dev/tty.usbmodem1201')
+# board = setup('/dev/tty.usbmodem1201')
+#board.digital[3].mode = pyfirmata.INPUT
+
+time.sleep(1)
+
+ser = serial.Serial('/dev/tty.usbmodem1201', 9600, timeout=0.1)
+
+while True:
+    line = ser.readline()
+
+    if line:
+        string = line.decode()
+        num = int(string)
+        print(num)
+
+ser.close()
+
 
