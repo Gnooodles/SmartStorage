e366f625ed77b47a1941f179ae13218b3027b60f
Daniele Gnudi <62075147+Gnooodles@users.noreply.github.com>
2023-08-17 15:40:03 +0200

    add docu

--- circuit snippets/gestore_database/lettura_seriale_e_database.py
+++ circuit snippets/gestore_database/lettura_seriale_e_database.py
@@ -2,74 +2,75 @@ import time
 import serial
 from setup_database import *
 
+# Establish a serial connection
 ser = serial.Serial("/dev/tty.usbmodem1201", 9600, timeout=0)
 SCAN_DURATION = 5
 
 
 def start_scan():
     """
-    Avvia una scansione inviando il comando appropriato alla sorgente seriale.
+    Start a scan by sending the appropriate command to the serial source.
 
-    Questa funzione invia un comando di scansione attraverso la sorgente seriale specificata
-    e attende una risposta. La scansione viene avviata con un breve ritardo di 0.1 secondi.
+    This function sends a scan command through the specified serial source
+    and waits for a response. The scan is initiated with a short delay of 0.1 seconds.
 
     Args:
-        ser (serial.Serial): L'oggetto Serial per la comunicazione seriale.
+        ser (serial.Serial): The Serial object for serial communication.
     """
     scan_command = b"\x7E\x00\x08\x01\x00\x02\x01\xAB\xCD"
     ser.write(scan_command)
     time.sleep(0.1)
 
     while True:
-        if ser.read().decode() == "1" or ser.read().decode() == "":
+        response = ser.read().decode()
+        if response == "1" or response == "":
             break
 
 
 def recieve_data() -> str:
     """
-    Riceve dati da una sorgente seriale fino al raggiungimento del timeout SCAN_DURATION.
+    Receive data from a serial source until the SCAN_DURATION timeout is reached.
 
-    Questa funzione legge i byte da una sorgente seriale fino a quando non viene rilevato
-    un carattere di ritorno a capo ('\r')
-    o fino a quando non è trascorso il tempo massimo di scansione definito da SCAN_DURATION.
+    This function reads bytes from a serial source until a carriage return ('\r') character
+    is detected or until the maximum scanning time defined by SCAN_DURATION is exceeded.
 
     Returns:
-        str: I dati ricevuti dalla sorgente seriale.
+        str: The data received from the serial source.
     """
     start_time = time.time()
-    data = ""
+    received_data = ""
 
     while (time.time() - start_time) <= SCAN_DURATION:
         byte = ser.read().decode()
         if byte == "\r":
             break
         else:
-            data += byte
+            received_data += byte
 
-    return data
+    return received_data
 
 
 status = ""
 
 while True:
     start_scan()
-    recieved_data = recieve_data()
+    received_data = recieve_data()
 
-    if recieved_data == "CARICA":
+    if received_data == "CARICA":
         status = "CARICA"
-    elif recieved_data == "SCARICA":
+    elif received_data == "SCARICA":
         status = "SCARICA"
-    elif recieved_data != "":
+    elif received_data != "":
         if status == "CARICA":
-            # carica su db
-            print(f"{status} - {recieved_data}")
-            add_item(recieved_data)
+            # Load product into the database
+            print(f"{status} - {received_data}")
+            add_item(received_data)
         elif status == "SCARICA":
-            # scarica dal db e mette nella lista
-            print(f"{status} - {recieved_data}")
-            remove_one_item(recieved_data)
+            # Remove product from the database
+            print(f"{status} - {received_data}")
+            remove_one_item(received_data)
         else:
-            # TODO caso in cui status è vuoto
+            # TODO: Handle case when status is empty
             pass
     else:
         pass
--- circuit snippets/gestore_database/setup_database.py
+++ circuit snippets/gestore_database/setup_database.py
@@ -1,41 +1,84 @@
 import sqlite3
 import os
 
+# Establish a connection to the SQLite database "magazzino.db"
 con = sqlite3.connect("magazzino.db")
 cur = con.cursor()
 
 
 def setup_database():
-    cur.execute("CREATE TABLE magazzino(barcode, name, quantity)")
+    """
+    Set up the initial database structure.
+
+    This function creates a table named 'magazzino' with columns for barcode, name, and quantity.
+    """
+    cur.execute(
+        "CREATE TABLE magazzino(barcode TEXT PRIMARY KEY, name TEXT, quantity INTEGER)"
+    )
 
 
 def add_item(barcode: str) -> None:
-    # TODO name = get_name_from_barcode(barcode)
+    """
+    Add an item to the database or update its quantity if it already exists.
+
+    Args:
+        barcode (str): The barcode of the item to be added.
+
+    If the item with the given barcode doesn't exist in the database, a new entry is added.
+    If the item already exists, its quantity is incremented by 1.
+    """
+    # TODO: Implement 'get_name_from_barcode' function to fetch the item's name based on the barcode.
     name = ""
-    if (
-        cur.execute(f"SELECT * FROM magazzino WHERE barcode = '{barcode}'").fetchone()
-        is None
-    ):
+
+    # Check if the item already exists in the database
+    existing_item = cur.execute(
+        f"SELECT * FROM magazzino WHERE barcode = '{barcode}'"
+    ).fetchone()
+
+    if existing_item is None:
+        # Insert a new item into the database with initial quantity of 1
         cur.execute(f"INSERT INTO magazzino VALUES ('{barcode}', '{name}', 1)")
-        con.commit()
     else:
+        # Increment the quantity of the existing item
         quantity = get_item_quantity(barcode)
         cur.execute(
-            f"UPDATE magazzino SET quantity = {quantity+1} WHERE barcode = '{barcode}'"
+            f"UPDATE magazzino SET quantity = {quantity + 1} WHERE barcode = '{barcode}'"
         )
-        con.commit()
+
+    # Commit the changes to the database
+    con.commit()
 
 
 def get_items():
+    """
+    Retrieve all items from the database.
+
+    Returns:
+        list: A list of tuples representing items in the format (barcode, name, quantity).
+    """
     res = cur.execute("SELECT * FROM magazzino")
     return res.fetchall()
 
 
 def erase_database():
+    """
+    Delete the database file "magazzino.db".
+
+    Caution: This operation is irreversible and will result in permanent data loss.
+    """
     os.remove("magazzino.db")
 
 
 def get_item_quantity(barcode: str) -> int:
+    """
+    Get the quantity of a specific item based on its barcode.
+
+    Args:
+        barcode (str): The barcode of the item.
+
+    Returns:
+        int: The quantity of the item.
+    """
     current_quantity = cur.execute(
         f"SELECT quantity FROM magazzino WHERE barcode = '{barcode}'"
     ).fetchone()[0]
@@ -43,19 +86,32 @@ def get_item_quantity(barcode: str) -> int:
 
 
 def remove_one_item(barcode: str):
-    if (
-        cur.execute(f"SELECT * FROM magazzino WHERE barcode = '{barcode}'").fetchone()
-        is not None
-    ):
+    """
+    Remove one quantity of the specified item from the database.
+
+    Args:
+        barcode (str): The barcode of the item to be removed.
+
+    If the item's quantity is greater than 1, its quantity is decremented by 1.
+    If the item's quantity is 1, the item is completely removed from the database.
+    """
+    existing_item = cur.execute(
+        f"SELECT * FROM magazzino WHERE barcode = '{barcode}'"
+    ).fetchone()
+
+    if existing_item is not None:
         quantity = get_item_quantity(barcode)
+
         if quantity == 1:
             cur.execute(f"DELETE FROM magazzino WHERE barcode = '{barcode}'")
-            con.commit()
         else:
             cur.execute(
-                f"UPDATE magazzino SET quantity = {quantity-1} WHERE barcode = '{barcode}'"
+                f"UPDATE magazzino SET quantity = {quantity - 1} WHERE barcode = '{barcode}'"
             )
-            con.commit()
+
+        # Commit the changes to the database
+        con.commit()
 
 
+# Call the setup_database function to initialize the database table
 # setup_database()
