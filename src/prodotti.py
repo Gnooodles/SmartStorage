6a88b417e0c013a029bee03673d0b0c6854b6522
Mattia Monti <77688571+mattiamonti@users.noreply.github.com>
2023-08-30 18:20:56 +0200

    implementato lo scraper per i barcode

--- src/prodotti.py
+++ src/prodotti.py
@@ -1,5 +1,7 @@
 import sqlite3
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 class Prodotti:
     def __init__(self, path: str) -> None:
@@ -24,6 +26,51 @@ class Prodotti:
         ).fetchone()
 
         if item_name is None:
-            return ""
+            # if the item's barcode is not finded in the database, search the barcode on the internet 
+            self.scrape_barcode_name(barcode)
 
         return item_name[0]
+    
+    def scrape_barcode_name(self, barcode: str) -> str:
+        """
+        Scrapes the name associated with a barcode from a Google search result.
+        And update the database with the barcode and the associate name finded.
+
+        Args:
+            barcode (str): The barcode for which the associated name needs to be scraped.
+
+        Returns:
+            str: The name associated with the given barcode. Returns an empty string if no result is found.
+        """
+        # setup driver and options
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+
+        # get the google search url for the barcode
+        driver.get(f"https://www.google.com/search?q={barcode}")
+        driver.implicitly_wait(1)
+
+        # find and click the button for the privacy consense pop-up
+        button = driver.find_element(By.ID, "L2AGLb")
+        button.click()
+        driver.implicitly_wait(2)
+
+        # find and return the first result, if None return an empty string
+        # first_result = driver.find_element(By.CSS_SELECTOR, ".tF2Cxc") # another way to get the first result
+        first_result = driver.find_element(By.CSS_SELECTOR, "h3.LC20lb")
+        driver.quit()
+
+        if first_result is None:
+            return ""
+        
+        result_name = first_result.text.split("\n")[0] # get the first line if it is a multiline
+        self._update_product(barcode, result_name)
+        return result_name
+    
+    def _update_product(self, barcode: str, name: str):
+        # TODO implementare la funzione per aggiornare il database 
+        # con il barcode e il nome trovato tramite lo scraper
+        #cur.execute(f"INSERT INTO prodotti VALUES ('{received_data}', '{name}')")
+        #con.commit()
+        pass
